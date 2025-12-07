4ce78e79f09f5145c636232e2997331c7e980b70
gokul537 <gokul@sataiva.com>
2025-12-07 14:58:29 +0530

    new file

---
+++ app/Haveanyqueries/page.jsx
@@ -0,0 +1,12 @@
+import Haveanyqueries from '@/page/Haveanyqueries'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <Haveanyqueries />
+    </div>
+  )
+}
+
+export default page
---
+++ app/track-your-package/page.jsx
@@ -0,0 +1,12 @@
+import Trackyourpackage from '@/page/Trackyourpackage'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <Trackyourpackage/>
+    </div>
+  )
+}
+
+export default page
