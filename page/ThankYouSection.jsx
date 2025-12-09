ef1a03155cfd86be931b408802ec0401cece2993
gokulkrishna1 <gokulkrishna@zethic.com>
2025-12-09 12:20:26 +0530

    thankyou page and not found page

--- page/ThankYouSection.jsx
+++ page/ThankYouSection.jsx
@@ -3,48 +3,51 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 import thankyou from "@/asset/thankyou.svg";
+import Link from "next/link";
+
 import TestimonialSliderRight from "@/components/TestimonialSliderRight";
 
 export default function ThankYouSection() {
   return (
-    <section className="w-full py-16 px-4">
+    <section className="w-full py-24 px-4">
       {/* TOP SECTION */}
       <motion.div
         initial={{ opacity: 0, y: -20 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
         viewport={{ once: true }}
-        className="text-center max-w-2xl mx-auto"
+        className="text-center max-w-2xl mx-auto pb-36"
       >
         <Image
           src={thankyou} // replace icon
           alt="Success icon"
-          width={60}
-          height={60}
+          width={160}
+          height={160}
           className="mx-auto"
         />
 
-        <h2 className="text-2xl md:text-3xl font-semibold mt-4">
+        <h2 className=" mt-4">
           <span className="text-blue-600">Thank you</span> for booking with us!
         </h2>
 
-        <p className="text-gray-600 mt-2 text-sm md:text-base">
+        <p className="text-second mt-4 ">
           We have successfully received your booking.
         </p>
-
-        <button className="mt-6 bg-blue-600 text-white px-6 py-2 rounded-full hover:bg-blue-700 transition">
-          Back to Home
-        </button>
+        <div className="mt-7">
+          <Link
+            href="/"
+            className="bg-primary text-lg text-white text-[16px] font-semibold py-3 px-10 rounded-full transition duration-300"
+          >
+            Back to Home
+          </Link>
+        </div>
       </motion.div>
 
-
-            <section className='mb-9 mt-7'>
-              <div className='max-w-7xl mx-auto'>
-                <TestimonialSliderRight />
-              </div>
-            </section>
-
-     
+      <section className="mb-9 mt-7">
+        <div className="max-w-7xl mx-auto">
+          <TestimonialSliderRight />
+        </div>
+      </section>
     </section>
   );
 }
