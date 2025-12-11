7b88cba2cf21fb8392a1af9f2ae19750366993cc
gokulkrishna1 <gokulkrishna@zethic.com>
2025-12-12 01:12:01 +0530

    new page design

---
+++ app/express-delivery-services/page.jsx
@@ -0,0 +1,12 @@
+import ExpressServices from '@/page/ExpressServices'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <ExpressServices />
+    </div>
+  )
+}
+
+export default page
---
+++ app/premium-delivery-services/page.jsx
@@ -0,0 +1,12 @@
+import PremiumServices from '@/page/PremiumServices'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <PremiumServices />
+    </div>
+  )
+}
+
+export default page
