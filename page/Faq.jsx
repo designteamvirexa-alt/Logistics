6990e47cad110ce9e1bfca035b72825d9bf853d1
gokulkrishna1 <gokulkrishna@zethic.com>
2025-12-18 22:50:47 +0530

    Faq

--- page/Faq.jsx
+++ page/Faq.jsx
@@ -1,12 +1,12 @@
 "use client";
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { motion } from "framer-motion";
-import MarqueeLogos from '@/components/MarqueeLogos';
-import Image from 'next/image';
+import MarqueeLogos from "@/components/MarqueeLogos";
+import Image from "next/image";
 import { ChevronRight, ChevronDown } from "lucide-react";
-import TestimonialSliderRight from '@/components/TestimonialSliderRight';
 import faqbg from "@/asset/shippment/faqs.webp";
-
+import CallToAction from "@/components/CallToAction";
+import Testimonials from "@/components/Testimonials";
 
 const menuItems = [
   "General FAQs",
@@ -21,20 +21,29 @@ const faqs = [
     q: "Where can I find my tracking (AWB) number?",
     a: "Your tracking number was shared via SMS and email when your order was booked. You can also get it from the sender or your order confirmation receipt.",
   },
-  { q: "Why is my shipment delayed?", a: "Your tracking number was shared via SMS and email when your order was booked. You can also get it from the sender or your order confirmation receipt." },
-  { q: "What should I do if my shipment status hasn’t updated?", a: "Your tracking number was shared via SMS and email when your order was booked. You can also get it from the sender or your order confirmation receipt." },
-  { q: "Can I change my delivery address after dispatch?", a: "Your tracking number was shared via SMS and email when your order was booked. You can also get it from the sender or your order confirmation receipt." },
-  { q: "How do I raise a complaint or report an issue?", a: "Your tracking number was shared via SMS and email when your order was booked. You can also get it from the sender or your order confirmation receipt." },
+  {
+    q: "Why is my shipment delayed?",
+    a: "Your tracking number was shared via SMS and email when your order was booked. You can also get it from the sender or your order confirmation receipt.",
+  },
+  {
+    q: "What should I do if my shipment status hasn’t updated?",
+    a: "Your tracking number was shared via SMS and email when your order was booked. You can also get it from the sender or your order confirmation receipt.",
+  },
+  {
+    q: "Can I change my delivery address after dispatch?",
+    a: "Your tracking number was shared via SMS and email when your order was booked. You can also get it from the sender or your order confirmation receipt.",
+  },
+  {
+    q: "How do I raise a complaint or report an issue?",
+    a: "Your tracking number was shared via SMS and email when your order was booked. You can also get it from the sender or your order confirmation receipt.",
+  },
 ];
 function Faq() {
-
-
   const [activeMenu, setActiveMenu] = useState("General FAQs");
   const [openIndex, setOpenIndex] = useState(0);
   return (
     <div>
-
-      <div className="-mt-20">
+      <div className="-mt-24 -mt-16">
         <motion.section
           initial={{ opacity: 0, y: -20 }}
           whileInView={{ opacity: 1, y: 0 }}
@@ -48,7 +57,7 @@ function Faq() {
             alt="About Background"
             fill
             priority
-            className="object-cover p-2 rounded-3xl"
+            className="object-cover p-3 rounded-3xl"
           />
 
           {/* Gradient Overlay */}
@@ -56,9 +65,7 @@ function Faq() {
 
           {/* CONTENT */}
           <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4">
-            <h2 className="text-white">
-             Frequently Asked Questions
-            </h2>
+            <h2 className="text-white">Frequently Asked Questions</h2>
 
             <p className="text-white/80 mt-3 text-sm md:text-base">
               Home <span className="text-blue-200">›</span> Faqs
@@ -67,35 +74,43 @@ function Faq() {
         </motion.section>
       </div>
 
-      <section className='max-w-7xl mx-auto py-20'>
+      <section className="max-w-7xl mx-auto py-14">
         <MarqueeLogos />
       </section>
 
-
-      <section className="w-full px-5 md:px-10 py-20">
+      <section className="w-full px-5 md:px-10 py-24">
         <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-10">
-
           {/* LEFT MENU (Fixed on Desktop) */}
           <div className="md:col-span-1">
-            <div className="
-            bg-white rounded-xl border shadow-sm p-4 
+            <div
+              className="
+            bg-white rounded-3xl  shadow-lg p-4 
             md:sticky md:top-24 md:h-fit
-          ">
+          "
+            >
               {menuItems.map((item) => (
                 <div
                   key={item}
                   onClick={() => setActiveMenu(item)}
                   className={`flex items-center justify-between p-3 cursor-pointer rounded-lg mb-2 
-                ${activeMenu === item ? "bg-blue-50 border border-blue-500" : "hover:bg-gray-100"}
+                ${
+                  activeMenu === item
+                    ? "bg-blue-50 border border-blue-500"
+                    : "hover:bg-gray-100"
+                }
               `}
                 >
                   <span
-                    className={`text-sm font-medium ${activeMenu === item ? "text-blue-600" : "text-gray-800"}`}
+                    className={`text-sm font-semibold ${
+                      activeMenu === item ? "text-primary" : "text-black"
+                    }`}
                   >
                     {item}
                   </span>
                   <ChevronRight
-                    className={`w-4 h-4 ${activeMenu === item ? "text-blue-600" : "text-gray-500"}`}
+                    className={`w-4 h-4 ${
+                      activeMenu === item ? "text-primary" : "text-gray-500"
+                    }`}
                   />
                 </div>
               ))}
@@ -109,25 +124,28 @@ function Faq() {
             transition={{ duration: 0.5 }}
             className="md:col-span-2"
           >
-            <h2 className="text-2xl font-semibold mb-5">{activeMenu}</h2>
+            <h3 className="font-semibold mb-5">{activeMenu}</h3>
 
             <div className="space-y-4">
               {faqs.map((faq, index) => (
                 <div
                   key={index}
-                  className="bg-white p-6 rounded-xl border shadow-sm"
+                  className="bg-white p-4 rounded-xl  border border-[#F5F5F5]"
                 >
                   {/* Question */}
                   <button
-                    onClick={() => setOpenIndex(openIndex === index ? -1 : index)}
+                    onClick={() =>
+                      setOpenIndex(openIndex === index ? -1 : index)
+                    }
                     className="flex w-full justify-between text-left"
                   >
-                    <span className="font-medium text-gray-900 text-sm md:text-base">
+                    <span className="font-semibold  text-black">
                       {faq.q}
                     </span>
                     <ChevronDown
-                      className={`w-5 h-5 transition-transform duration-300 ${openIndex === index ? "rotate-180" : ""
-                        }`}
+                      className={`w-5 h-5 transition-transform duration-300 ${
+                        openIndex === index ? "rotate-180" : ""
+                      }`}
                     />
                   </button>
 
@@ -137,7 +155,7 @@ function Faq() {
                       initial={{ opacity: 0, height: 0 }}
                       animate={{ opacity: 1, height: "auto" }}
                       exit={{ opacity: 0, height: 0 }}
-                      className="text-gray-600 mt-3 text-sm"
+                      className="text-second mt-3 text-sm"
                     >
                       {faq.a}
                     </motion.p>
@@ -145,23 +163,26 @@ function Faq() {
                 </div>
               ))}
             </div>
-            <div className="space-y-4">
+            <div className="space-y-4 mt-4">
               {faqs.map((faq, index) => (
                 <div
                   key={index}
-                  className="bg-white p-6 rounded-xl border shadow-sm"
+                  className="bg-white p-4 rounded-xl  border border-[#F5F5F5]"
                 >
                   {/* Question */}
                   <button
-                    onClick={() => setOpenIndex(openIndex === index ? -1 : index)}
+                    onClick={() =>
+                      setOpenIndex(openIndex === index ? -1 : index)
+                    }
                     className="flex w-full justify-between text-left"
                   >
-                    <span className="font-medium text-gray-900 text-sm md:text-base">
+                    <span className="font-semibold  text-black">
                       {faq.q}
                     </span>
                     <ChevronDown
-                      className={`w-5 h-5 transition-transform duration-300 ${openIndex === index ? "rotate-180" : ""
-                        }`}
+                      className={`w-5 h-5 transition-transform duration-300 ${
+                        openIndex === index ? "rotate-180" : ""
+                      }`}
                     />
                   </button>
 
@@ -171,7 +192,7 @@ function Faq() {
                       initial={{ opacity: 0, height: 0 }}
                       animate={{ opacity: 1, height: "auto" }}
                       exit={{ opacity: 0, height: 0 }}
-                      className="text-gray-600 mt-3 text-sm"
+                      className="text-second mt-3 text-sm"
                     >
                       {faq.a}
                     </motion.p>
@@ -183,14 +204,11 @@ function Faq() {
         </div>
       </section>
 
-      <section className='py-20'>
-        <div className='max-w-7xl mx-auto'>
-          <TestimonialSliderRight />
-        </div>
-      </section>
+      <Testimonials />
 
+      <CallToAction />
     </div>
-  )
+  );
 }
 
-export default Faq
+export default Faq;
