2118c5a9eacea95a66d7d56ee9e1d1e5fdafa6ba
gokulkrishna1 <gokulkrishna@zethic.com>
2026-01-01 02:31:40 +0530

    image added

--- page/BookShipment.jsx
+++ page/BookShipment.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 import MarqueeLogos from "@/components/MarqueeLogos";
 import ShipmentForm from "@/components/ShipmentForm";
 import FAQ from "@/components/Accordion";
-import bookshipment from "@/asset/shippment/bookshipment.webp";
+import bookshipment from "@/asset/book-shipment.png";
 import Link from "next/link";
 
 function BookShipment() {
@@ -17,7 +17,7 @@ function BookShipment() {
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6 }}
           viewport={{ once: true }}
-          className="relative w-full h-[460px] md:h-[460px] rounded-3xl overflow-hidden"
+          className="relative w-full h-[460px] md:h-[460px] overflow-hidden"
         >
           {/* Background Image */}
           <Image
@@ -25,26 +25,24 @@ function BookShipment() {
             alt="About Background"
             fill
             priority
-            className="object-cover p-3  rounded-3xl"
+            className="md:object-contain object-cover bg-center"
           />
 
           {/* Gradient Overlay */}
           {/* <div className="absolute inset-0 bg-gradient-to-b from-black/10 via-black/40 to-blue-700/90"></div> */}
 
           {/* CONTENT */}
-          <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4">
-            <h2 className="text-white text-3xl md:text-4xl ">Book Shipment</h2>
+          <div className="relative z-10 flex flex-col items-center justify-center h-full text-center px-4 max-w-3xl mx-auto">
+            <h1 className="text-black text-3xl md:text-4xl ">Book Shipment</h1>
 
-            <p className=" mt-3 text-white">
-              Home <span className="text-white">›</span> Book Shipment
-            </p>
+           
           </div>
         </motion.section>
       </div>
 
-      <div className="lg:py-16 py-16 max-w-7xl mx-auto">
+      <section className="container mx-auto px-4 py-24 pb-0 ">
         <MarqueeLogos />
-      </div>
+      </section>
 
       <div>
         <ShipmentForm />
